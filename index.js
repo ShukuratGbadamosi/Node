b31dead964bd3527d2a5fc2cdd10b64d71457044
gbadamosi shukurat opeyemi <gbadamosishukurat2006@gmail.com>
2024-08-08 09:48:03 +0100

    chore: Add welcome message to homepage

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const expressUse = require("express");
+
 const app = expressUse();
 const PORT = 3000;
 const HOSTNAME = "localhost"
+const name = "shukkie"
 
 // app.listen(number(port number), callBackFunction)
+
+app.get("/", (req,res)=>{
+    res.send(`"welcome to my website  ${name}"`)
+    console.log(`"hello shukkie`)
+})
 
 app.listen(PORT, HOSTNAME, ()=> {
     console.log("I am a server and am running on port 3000")
